b2bb9f9206b890d5bc5e3d66fdcf4a85fbdca8fd
Andre Louis Caron <andre.louis.caron@usherbrooke.ca>
2011-10-24 04:00:50 +0000

    Demo project for pre-fork server.

---
+++ demo/prefork-echo-server/prefork-echo-server.cpp
@@ -0,0 +1,53 @@
+// Copyright(c) 2009-2011 Andre Caron (andre.l.caron@gmail.com)
+//
+// This document is covered by the Artistic License 2.0 (Open Source Initiative
+// approved license). A copy of the license should have been provided alongside
+// this software package (see "license.rtf"). If not, the license is available
+// online at "http://www.opensource.org/licenses/artistic-license-2.0.php".
+
+#include <w32.hpp>
+#include <w32.net.hpp>
+
+#include <w32/net/prefork-server.hpp>
+
+namespace {
+
+    class Server
+    {
+        /* construction. */
+    public:
+        Server ()
+        {
+        }
+
+        /* operators. */
+    public:
+        bool operator() ( w32::net::tcp::Stream& stream )
+        {
+              // allocate buffer for this connection.
+            char data[16*1024];
+            
+              // echo data sent by peer until peer is fed up and quits.
+            for ( w32::dword size; (size=stream.get(data,sizeof(data))) > 0; )
+            {
+                std::cout
+                    << "Received '" << size << "' bytes."
+                    << std::endl;
+                for ( w32::dword used = 0; (used < size); ) {
+                    used += stream.put(data+used, size-used);
+                }
+            }
+            
+              // allow accepting infinite connections.
+            return (true);
+        }
+    };
+
+}
+
+#include <w32/net/prefork-server.cpp>
+
+#pragma comment ( lib, "w32.lib" )
+#pragma comment ( lib, "w32.dbg.lib" )
+#pragma comment ( lib, "w32.io.lib" )
+#pragma comment ( lib, "w32.net.lib" )
