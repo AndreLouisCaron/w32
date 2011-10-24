b001c5d9aada714e215761cd18b17d859c094db9
Andre Louis Caron <andre.louis.caron@usherbrooke.ca>
2011-10-24 00:36:55 +0000

    Demonstration of socket inheritance.

---
+++ demo/prefork-master/prefork-master.cpp
@@ -0,0 +1,125 @@
+// Copyright(c) 2009-2011 Andre Caron (andre.l.caron@gmail.com)
+//
+// This document is covered by the Artistic License 2.0 (Open Source Initiative
+// approved license). A copy of the license should have been provided alongside
+// this software package (see "license.rtf"). If not, the license is available
+// online at "http://www.opensource.org/licenses/artistic-license-2.0.php".
+
+#include <w32.hpp>
+#include <w32.ipc.hpp>
+#include <w32.net.hpp>
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+namespace {
+
+    w32::string slave_command
+        ( const w32::string& path, w32::net::Socket::Handle handle )
+    {
+        std::wostringstream command;
+        command
+            << '"' << path << '"' << " " << handle;
+        return (command.str().c_str());
+    }
+
+}
+
+#include <w32/console-program.hpp>
+
+namespace {
+
+    int run ( int argc, wchar_t ** argv )
+    {
+          // prepare sockets library.
+        const w32::net::Context _;
+        
+          // configure master process.
+        const w32::net::ipv4::EndPoint endpoint
+            (w32::net::ipv4::Address::any(), 4321);
+        const w32::dword slavecount = 1;
+        const w32::string slavepath = L"../../.build/dbg/prefork-slave.exe";
+        
+        std::wcout
+            << L"Online in: '" << w32::currentdirectory() << "'"
+            << std::endl;
+        
+        std::cout
+            << "Opening listening socket."
+            << std::endl;
+        
+          // start listening for connections.
+        w32::net::tcp::Listener listener(endpoint);
+        
+        std::cout
+            << "Starting slaves."
+            << std::endl;
+        
+          // create slave processes.
+        const w32::string slavecommand =
+            ::slave_command(slavepath, listener.handle());
+        std::vector<w32::ipc::Process> slaves;
+        slaves.reserve(slavecount);
+        w32::Waitable::Set objects;
+        for ( w32::dword i = 0; (i < slavecount); ++i ) {
+            const w32::ipc::Process slave(slavecommand, true);
+            slaves.push_back(slave), objects |= slave.handle();
+        }
+        
+        std::cout
+            << "Monitoring slaves."
+            << std::endl;
+        
+        bool running = false;
+        
+          // monitor slave processes and re-spawn when necessary.
+        do {
+              // wait for any slave process to complete.
+            const w32::dword i = w32::Waitable::any(objects);
+              // check slave process status, log errors.
+            const w32::dword status = slaves[i].status();
+            if ( status == 0 )
+            {
+                std::cout
+                    << "Slave process finished."
+                    << std::endl;
+            }
+            else
+            {
+                std::cerr
+                    << "Slave process crashed (" << status << ")."
+                    << std::endl;
+            }
+            
+              // replace slave process with fresh instance.
+            objects &= slaves[i].handle();
+            if ( running ) {
+                slaves[i] = w32::ipc::Process(slavecommand, true);
+                objects |= slaves[i].handle();
+            }
+        }
+        while ( running );
+        
+        std::cout
+            << "Waiting for slaves."
+            << std::endl;
+        if ( !objects.empty() ) {
+            w32::Waitable::all(objects);
+        }
+        std::cout
+            << "Over and out."
+            << std::endl;
+        
+        return (EXIT_SUCCESS);
+    }
+
+}
+
+#include <w32/console-program.cpp>
+
+#pragma comment ( lib, "w32.lib" )
+#pragma comment ( lib, "w32.dbg.lib" )
+#pragma comment ( lib, "w32.mt.lib" )
+#pragma comment ( lib, "w32.ipc.lib" )
+#pragma comment ( lib, "w32.net.lib" )
---
+++ demo/prefork-slave/prefork-slave.cpp
@@ -0,0 +1,80 @@
+// Copyright(c) 2009-2011 Andre Caron (andre.l.caron@gmail.com)
+//
+// This document is covered by the Artistic License 2.0 (Open Source Initiative
+// approved license). A copy of the license should have been provided alongside
+// this software package (see "license.rtf"). If not, the license is available
+// online at "http://www.opensource.org/licenses/artistic-license-2.0.php".
+
+#include <w32.hpp>
+#include <w32.net.hpp>
+
+#include <iostream>
+#include <sstream>
+
+namespace {
+
+    w32::net::tcp::Listener listener ( const wchar_t * argument )
+    {
+        ::SOCKET handle = INVALID_SOCKET;
+        std::wistringstream parser(argument);
+        if ( !(parser >> handle) ) {
+            throw (std::exception("Could not parser listening socket handle."));
+        }
+        return (w32::net::tcp::Listener(w32::net::Socket::proxy(handle)));
+    }
+
+}
+
+#include <w32/console-program.hpp>
+
+namespace {
+
+    int run ( int argc, wchar_t ** argv )
+    {
+          // prepare sockets library.
+        const w32::net::Context _;
+        
+          // inherit listening socket handle from master process.
+        if ( argc < 2 ) {
+            throw (std::exception("Missing argument!"));
+        }
+        w32::net::tcp::Listener listener = ::listener(argv[1]);
+        
+        char data[16*1024];
+        
+            // accept connections from gateway.
+        do {
+            w32::net::tcp::Stream stream(listener);
+            
+            std::cout
+                << "Accepted connection."
+                << std::endl;
+            
+              // process data on transport connection.
+            for ( w32::dword size; (size = stream.get(data,sizeof(data))) > 0; )
+            {
+                std::cout
+                    << "Received '" << size << "' bytes."
+                    << std::endl;
+                for ( w32::dword used = 0; (used < size); ) {
+                    used += stream.put(data+used, size-used);
+                }
+            }
+            
+            std::cout
+                << "Client disconnected."
+                << std::endl;
+        }
+        while ( false );
+        
+        return (EXIT_SUCCESS);
+    }
+
+}
+
+#include <w32/console-program.cpp>
+
+#pragma comment ( lib, "w32.lib" )
+#pragma comment ( lib, "w32.dbg.lib" )
+#pragma comment ( lib, "w32.io.lib" )
+#pragma comment ( lib, "w32.net.lib" )
