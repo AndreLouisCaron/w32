ad5c02d9b0d6674c6a4996e86249384875fc1584
Andre Louis Caron <andre.louis.caron@usherbrooke.ca>
2011-10-24 04:00:15 +0000

    Class-based model for pre-fork server to support initialization.

---
+++ code/w32/net/prefork-server.hpp
@@ -0,0 +1,17 @@
+#ifndef _w32_net_preforkserver_hpp__
+#define _w32_net_preforkserver_hpp__
+
+// Copyright(c) 2009-2011 Andre Caron (andre.l.caron@gmail.com)
+//
+// This document is covered by the Artistic License 2.0 (Open Source Initiative
+// approved license). A copy of the license should have been provided alongside
+// this software package (see "license.rtf"). If not, the license is available
+// online at "http://www.opensource.org/licenses/artistic-license-2.0.php".
+
+namespace {
+
+    class Server;
+
+}
+
+#endif /* _w32_net_preforkserver_hpp__ */
