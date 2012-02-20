4ff9b20a2dbdbc801ba4bc695b615ddb9e93a1c7
Andre Caron <andre.l.caron@gmail.com>
2012-02-20 17:13:36 -0500

    Enhances documentation.

--- code/w32.tp/Queue.hpp
+++ code/w32.tp/Queue.hpp
@@ -40,16 +40,27 @@ namespace w32 { namespace tp {
 
     /*!
      * @ingroup w32-tp
-     * @brief Dispatch queue for a group of threads.
+     * @brief Dispatch queue for a thread pool.
      */
     class Queue :
         private NotCopyable
     {
         /* nested types. */
     public:
+        /*!
+         * @brief Native representation of the callback queue.
+         */
         typedef ::TP_CALLBACK_ENVIRON Data;
 
-        typedef void (__stdcall*Cancel)(void*,void*);
+        /*!
+         * @brief Native cleanupcallback signature.
+         */
+        typedef void(__stdcall*Cancel)(void*,void*);
+
+        /*!
+         * @brief Native signature for simple callback.
+         */
+        typedef void(__stdcall*Callback)(::PTP_CALLBACK_INSTANCE,void*);
 
         template<void(*)(Hints&,void*)> struct function;
         template<typename T, void(T::*M)(Hints&)> struct method;
@@ -60,16 +71,54 @@ namespace w32 { namespace tp {
 
         /* construction. */
     public:
+        /*!
+         * @brief Create a dispatch queue that uses @a pool's threads.
+         */
         Queue ( Pool& pool );
+
+        /*!
+         * @brief Destroy the callback queue.
+         *
+         * @warning Invocation of any pending callbacks after the destructor
+         *  has started execution results in undefined behavior.  Make sure
+         *  all callback registrations are cancelled and all callback
+         *  notifications are flushed before the queue is destroyed.
+         */
         ~Queue ();
 
         /* methods. */
     public:
+        /*!
+         * @brief Obtain read-write access to the native representation.
+         */
         Data& data ();
+
+        /*!
+         * @brief Obtain read-only access to the native representation.
+         */
         const Data& data () const;
 
+        /*!
+         * @brief Registers a cleanup group for the callback queue.
+         * @param cleanup The cleanup context.
+         * @param cancel Cancellation function that is invoked once for each
+         *  thread pool resource destroyed by the cleanup group.
+         *
+         * @warning Do @e not register a cleanup group if you intend to cleanup
+         *  after the thread pool resources using other means (e.g. the @c Wait,
+         *  @c Work, @c Timer and @c Transfer classes' destructors) because
+         *  registration of a cleanup group destroys all affiliated thread pool
+         *  resources automatically.  Registration of a cleanup group with
+         *  managed thread pool resources results in undefined behavior.
+         */
         void cleanup ( Cleanup& cleanup, Cancel cancel = 0 );
 
+        /*!
+         * @brief Schedule execution of a callback.
+         * @param context Context pointer passed to @a function.
+         * @param function Function that executes the work from a thread pool
+         *  thread.
+         */
         template<void(*F)(Hints&,void*)>
         void submit ( void * context, function<F> function )
         {
@@ -82,6 +131,12 @@ namespace w32 { namespace tp {
             }
         }
 
+        /*!
+         * @brief Schedule execution of a callback.
+         * @param object Context object used to invoke @a method.
+         * @param method Function that executes the work from a thread pool
+         *  thread.
+         */
         template<typename T, void(T::*M)(Hints&)>
         void submit ( T& object, method<T,M> method )
         {
@@ -95,11 +150,16 @@ namespace w32 { namespace tp {
         }
     };
 
-    // compile-time binding of free function to callback function.
+    /*!
+     * @brief Compile-time binding of free function to callback function.
+     */
     template<void(*F)(Hints&,void*)>
     struct Queue::function
     {
-        operator ::PTP_SIMPLE_CALLBACK () const
+        /*!
+         * @brief Convert to a function pointer with the native signature.
+         */
+        operator Queue::Callback () const
         {
             return (&simple_callback);
         }
@@ -108,15 +168,28 @@ namespace w32 { namespace tp {
         static void __stdcall simple_callback
             ( ::PTP_CALLBACK_INSTANCE instance, void * context )
         {
-            F(Hints(instance), context);
+            try {
+                F(Hints(instance), context);
+            }
+            catch ( ... )
+            {
+                std::cerr
+                    << "Exception raised from work handler."
+                    << std::endl;
+            }
         }
     };
 
-    // compile-time binding of object method to callback function.
+    /*!
+     * @brief Compile-time binding of member function to callback function.
+     */
     template<typename T, void(T::*M)(Hints&)>
     struct Queue::method
     {
-        operator ::PTP_SIMPLE_CALLBACK () const
+        /*!
+         * @brief Convert to a function pointer with the native signature.
+         */
+        operator Queue::Callback () const
         {
             return (&simple_callback);
         }
